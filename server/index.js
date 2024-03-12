f4677e3109505aaca28062691f26efc9c47f2150
pleo2 <leooel23m@gmail.com>
2024-03-12 17:10:41 -0400

    add socket.io and create index js  create server with socket.io and create server of node:http

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const app = express()
+const server = createServer(app) // crear un servido
+const io = new Server(server)
+
+io.on('connection', () => {
+	console.log('a user has connect')
+})
+
 app.use(logger('dev'))
 
 app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
@@ -10,6 +20,7 @@ const PORT = process.env.PORT ?? 3000
 
 app.get('/', (req, res) => {
 	// res.sendFile(process.cwd() + 'client/index.html')	
+	
 	res.sendFile(process.cwd() + '/client/index.html')
 })
 
@@ -17,6 +28,10 @@ app.get('/output.css', (req,res) => {
 	res.sendFile(process.cwd() + '/client/output.css')
 })
 
-app.listen(PORT, () => {
+app.get('/index.js', (req,res) => {
+	res.sendFile(process.cwd() + '/client/index.js')
+})
+
+server.listen(PORT, () => {
 	console.log(`server listening on port http://localhost:${PORT}`)
 })
