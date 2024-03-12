c53022baecc971ac24f1ae6a2fb6b08b7924c2af
pleo2 <leooel23m@gmail.com>
2024-03-12 15:28:55 -0400

    add morgan loader

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import express from 'express'
 const app = express()
 app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
 // eslint-disable-next-line
-const PORT = process.env.PORT ?? 1234
+const PORT = process.env.PORT ?? 3000
+
+app.get('/', (req, res) => {
+	res.send('<h1>hello world</h1>')
+})
 
 app.listen(PORT, () => {
 	console.log(`server listening on port http://localhost:${PORT}`)
