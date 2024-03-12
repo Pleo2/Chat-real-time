0628547e0c1135bc2a752e35ecac465166ce5887
pleo2 <leooel23m@gmail.com>
2024-03-12 16:55:36 -0400

    add talwind css and add server routes for html and css

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 import express from 'express'
+import logger from 'morgan'
 
 const app = express()
+app.use(logger('dev'))
+
 app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
 // eslint-disable-next-line
 const PORT = process.env.PORT ?? 3000
 
 app.get('/', (req, res) => {
-	res.send('<h1>hello world</h1>')
+	// res.sendFile(process.cwd() + 'client/index.html')	
+	res.sendFile(process.cwd() + '/client/index.html')
+})
+
+app.get('/output.css', (req,res) => {
+	res.sendFile(process.cwd() + '/client/output.css')
 })
 
 app.listen(PORT, () => {
